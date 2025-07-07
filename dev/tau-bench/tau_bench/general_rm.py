e19474bd04d65b20db42a1353a13f1c3131d617c
Saumya Gandhi <saumya@Saumyas-MacBook-Pro.local>
2025-07-07 11:58:00 -0700

    add llm judge

--- dev/tau-bench/tau_bench/general_rm.py
+++ dev/tau-bench/tau_bench/general_rm.py
@@ -1,11 +1,13 @@
 import copy
-from tau_bench.types import RunConfig
+from tau_bench.types import RunConfig, SolveResult
 import art
 from pydantic import BaseModel, Field
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List, Tuple, Type, TypeVar
 from openai.types.chat.chat_completion import Choice
 from openai import AsyncOpenAI
 from tau_bench.rl_utils import update_openpipe_log
+
+T = TypeVar("T")
 
 
 class RolloutScore(BaseModel):
@@ -14,6 +16,19 @@ class RolloutScore(BaseModel):
     """
 
     rollout_index: int = Field(description="Index of the rollout being scored")
+    explanation: str = Field(
+        description="Detailed explanation of what the rollout did well and what it did poorly, as determined by grading standards"
+    )
+    score: float = Field(
+        description="Numerical score between 0 and 1 indicating rollout quality, as determined by grading standards and explanation"
+    )
+
+
+class RolloutScoreLLM(BaseModel):
+    """
+    Model representing the score and explanation for a single rollout, for the LLM reward type.
+    """
+
     explanation: str = Field(
         description="Detailed explanation of what the rollout did well and what it did poorly, as determined by grading standards"
     )
@@ -44,6 +59,18 @@ For each rollout, you need to output the rollout index, the explanation of the s
 
 """
 
+LLM_JUDGE_SINGLE_ROLLOUT_PROMPT = """
+You are a helpful assistant that judges the quality of a customer support agent based on a single rollout of a conversation between a user and a customer support agent. You will be given the following information:
+- The system prompt that was provided at the beginning of the rollout to the assistant. Understand how the agent is supposed to behave based on this prompt.
+- The initial objective of the user that they set out to achieve with the assistant.
+- The correct order and set of tools that the assistant could have used to achieve the user's objective. This is not necessarily the only way, but definitely a correct way.
+- The rollout of the conversation between the user and the assistant.
+
+Output a score between 0 and 1 indicating the quality of the assistant's performance. 0 is the lowest score, 1 is the highest score. A rollout that completes the task partially should get credit accordingly, based on how well it did.
+
+Output your score and a detailed explanation of your score.
+"""
+
 
 def keep_only_messages(
     messages_and_choices: art.MessagesAndChoices,
@@ -71,13 +98,13 @@ def create_and_split_messages(
 
 
 async def create_openai_response(
-    prompt: str, judge_model: str = "o3"
-) -> RolloutScores | None:
+    prompt: str, judge_model: str = "o3", response_format: Type[T] = RolloutScores
+) -> T | None:
     async with AsyncOpenAI() as client:
         response = await client.beta.chat.completions.parse(
             model=judge_model,
             messages=[{"role": "user", "content": prompt}],
-            response_format=RolloutScores,
+            response_format=response_format,
         )
         return response.choices[0].message.parsed
 
@@ -96,27 +123,14 @@ async def create_general_rm_trajectory_groups(
             user_prompt += f"\n\n--- ROLLOUT {idx} ---\n"
             _, messages = create_and_split_messages(trajectory.messages_and_choices)
             # Format conversation
-            for msg in messages:
-                role = msg.get("role", "unknown")
-                content = msg.get("content", "")
-                tool_calls = msg.get("tool_calls", [])
+            user_prompt = add_messages_to_prompt(user_prompt, messages)
 
-                if tool_calls and len(tool_calls) > 0:
-                    tool_call_str = ""
-                    for tool_call in tool_calls:
-                        tool_call_str += f"TOOL CALL: {tool_call['function']['name']}: {tool_call['function']['arguments']}\n"
-                    user_prompt += f"{role.upper()}: {tool_call_str}\n"
-                else:
-                    user_prompt += f"{role.upper()}: {content}\n"
-
-        if config.general_rm_model.startswith("o3"):
-            response = await create_openai_response(
-                user_prompt, config.general_rm_model
-            )
+        if config.judge_model.startswith("o3") or config.judge_model.startswith(
+            "o4-mini"
+        ):
+            response = await create_openai_response(user_prompt, config.judge_model)
         else:
-            raise ValueError(
-                f"General RM model {config.general_rm_model} not supported"
-            )
+            raise ValueError(f"General RM model {config.judge_model} not supported")
 
         assert response is not None
         assert len(response.rollout_scores) == len(group.trajectories)
@@ -142,3 +156,72 @@ async def create_general_rm_trajectory_groups(
     except Exception as e:
         print(f"Error creating general RM trajectory groups: {e}")
         return group
+
+
+def add_messages_to_prompt(user_prompt: str, messages: List[Dict[str, Any]]) -> str:
+    for msg in messages:
+        role = msg.get("role", "unknown")
+        content = msg.get("content", "")
+        tool_calls = msg.get("tool_calls", [])
+
+        if tool_calls and len(tool_calls) > 0:
+            tool_call_str = ""
+            for tool_call in tool_calls:
+                tool_call_str += f"TOOL CALL: {tool_call['function']['name']}: {tool_call['function']['arguments']}\n"
+            user_prompt += f"{role.upper()}: {tool_call_str}\n"
+        else:
+            user_prompt += f"{role.upper()}: {content}\n"
+    return user_prompt
+
+
+def create_correct_order_and_set_of_tools(actions: List[Dict[str, Any]]) -> str:
+    """
+    Create a string representation of the correct order and set of tools that the assistant could have used to achieve the user's objective.
+    """
+    resp = ""
+    for action in actions:
+        resp += f"TOOL CALL: {action['name']}: {action['kwargs']}\n"
+    return resp
+
+
+async def calculate_reward(result: SolveResult, config: RunConfig) -> Tuple[float, str]:
+    # If the agent was forced to stop, it should get a score of -1, regardless of the reward type
+    if result.info["forced_stop"]:
+        return -1, "Max token trajectory"
+
+    if config.reward_type == "general_rm":
+        # general rm reward is calculated in reference to the other rollouts in the group
+        return result.reward, "pending_general_rm_reward"
+
+    reward = 0.0
+
+    # add the verifiable reward from the environment directly
+    reward += result.reward
+
+    if config.reward_type == "real":
+        return reward, "real_reward"
+
+    if config.reward_type == "real+llm":
+        try:
+            user_objective = result.info["task"]["instruction"]
+            correct_order_and_set_of_tools = create_correct_order_and_set_of_tools(
+                result.info["task"]["actions"]
+            )
+            user_prompt = LLM_JUDGE_SINGLE_ROLLOUT_PROMPT
+            user_prompt += f"\n\nSystem Prompt:\n--- START OF SYSTEM PROMPT ---\n{result.messages[0]['content']}\n--- END OF SYSTEM PROMPT ---\n\n"
+            user_prompt += f"\n User Objective:\n--- START OF USER OBJECTIVE ---\n{user_objective}\n--- END OF USER OBJECTIVE ---\n\n"
+            user_prompt += f"\n Correct Order and Set of Tools:\n--- START OF CORRECT TOOLS ---\n{correct_order_and_set_of_tools}\n--- END OF CORRECT TOOLS ---\n\n"
+            user_prompt += "\n Rollout:\n--- START OF ROLLOUT ---\n"
+            user_prompt = add_messages_to_prompt(user_prompt, result.messages)
+            user_prompt += "\n--- END OF ROLLOUT ---\n\n"
+            response = await create_openai_response(
+                user_prompt, config.judge_model, response_format=RolloutScoreLLM
+            )
+            assert response is not None
+            reward += response.score
+            return reward, response.explanation
+        except Exception as e:
+            print(f"Error calculating LLM reward: {e}")
+            return reward, "real_reward, error_calculating_llm_reward: " + str(e)
+
+    raise ValueError(f"Invalid reward type: {config.reward_type}")
