8dda1727148d69d790d2ba5a81c2fac9edd2508a
arcticfly <davidlcorbitt@gmail.com>
2025-07-24 13:31:13 -0700

    Generate comparison benchmarks

--- examples/art-mcp/art_mcp/benchmarks/generate_benchmarks.py
+++ examples/art-mcp/art_mcp/benchmarks/generate_benchmarks.py
@@ -8,6 +8,8 @@ import json
 from typing import List
 import weave
 
+from art.rewards.ruler import ruler_score_group
+
 from ..rollout import McpScenario, rollout
 from servers.python.mcp_alphavantage.server_params import server_params
 
@@ -66,9 +68,11 @@ sonnet_4 = art.Model(
 
 
 async def log_comparison_model(
-    comparison_model: art.Model, val_scenarios: List[McpScenario]
-):
-    trajectories = await art.gather_trajectory_groups(
+    comparison_model: art.Model,
+    val_scenarios: List[McpScenario],
+    control_groups: list[art.TrajectoryGroup] | None = None,
+) -> list[art.TrajectoryGroup]:
+    groups = await art.gather_trajectory_groups(
         (
             art.TrajectoryGroup(
                 rollout(comparison_model, val_scenarios[i]) for _ in range(4)
@@ -79,13 +83,38 @@ async def log_comparison_model(
         max_exceptions=1,
     )
 
+    if control_groups is not None:
+        for i in range(len(groups)):
+            for j in range(len(groups[i].trajectories)):
+                group = art.TrajectoryGroup(
+                    [
+                        control_groups[i].trajectories[j],
+                        groups[i].trajectories[j],
+                    ]
+                )
+                await ruler_score_group(
+                    group,
+                    judgement_model="openai/o4-mini",
+                )
+                control_score = group.trajectories[0].metrics["reward"]
+                benchmark_score = group.trajectories[1].metrics["reward"]
+
+                if benchmark_score > control_score:
+                    groups[i].trajectories[j].metrics["beat_comp"] = 1
+                elif benchmark_score < control_score:
+                    groups[i].trajectories[j].metrics["beat_comp"] = 0
+                else:
+                    groups[i].trajectories[j].metrics["beat_comp"] = 0.5
+
     await comparison_model.log(
-        trajectories,
+        groups,
         split="val",
     )
     await backend._experimental_push_to_s3(
         comparison_model,
     )
+
+    return groups
 
 
 async def run_benchmarks():
@@ -106,20 +135,17 @@ async def run_benchmarks():
     await o4_mini.register(backend)
     await sonnet_4.register(backend)
 
-    promises = []
+    control_groups = await log_comparison_model(gpt_4o_mini, val_scenarios)
 
     for comparison_model in [
         gpt_4o_mini,
         gpt_4o,
         gpt_41,
-        o3,
-        o4_mini,
-        sonnet_4,
+        # o3,
+        # o4_mini,
+        # sonnet_4,
     ]:
-        await log_comparison_model(comparison_model, val_scenarios)
-    #     promises.append(log_comparison_model(comparison_model, val_scenarios))
-
-    # await asyncio.gather(*promises)
+        await log_comparison_model(comparison_model, val_scenarios, control_groups)
 
 
 if __name__ == "__main__":
